b06e6e760a26978f5e388ef606dd881f592be670
zhou.zhang <zhou.zhang@weimob.com>
2018-12-25 16:28:03 +0800

    add entity

---
+++ src/main/java/com/weimob/SpringGit2Application.java
@@ -0,0 +1,14 @@
+package com.weimob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGit2Application.class, args);
+    }
+
+}
+
